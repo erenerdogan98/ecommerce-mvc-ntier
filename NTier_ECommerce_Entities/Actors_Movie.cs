aa942ef7b04ea4327a3a1344ff53bc2b90bc023e
erenerdogan98 <eren.erdogannn@hotmail.com>
2023-12-25 05:01:55 +0300

    Acotrs_Movie models created , it has relation Actors and Movie

---
+++ NTier_ECommerce_Entities/Actors_Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier_ECommerce_Entities
+{
+    public class Actors_Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+        public int ActorId { get; set; }
+        public Actors Actor { get; set; }
+    }
+}
