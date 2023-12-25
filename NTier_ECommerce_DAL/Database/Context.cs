5d3db9cbfe1a408f340b8646bd56f7cf87366445
erenerdogan98 <eren.erdogannn@hotmail.com>
2023-12-25 14:12:19 +0300

    Initial migration created  and updated database

--- NTier_ECommerce_DAL/Database/Context.cs
+++ NTier_ECommerce_DAL/Database/Context.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NTier_ECommerce_DAL.Context
+namespace NTier_ECommerce_DAL.Database
 {
     public class Context : DbContext
     {
