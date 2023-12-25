fbd5a175f4421761ddfffa5d46ab778e2a36c541
erenerdogan98 <eren.erdogannn@hotmail.com>
2023-12-25 05:06:37 +0300

    changing props and added realtionships

--- NTier_ECommerce_Entities/Movie.cs
+++ NTier_ECommerce_Entities/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,26 @@ namespace NTier_ECommerce_Entities
     {
         [Key]
         public int Id { get; set; }
-        public string Logo { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string MovieName { get; set; }
+        public string MovieDescription { get; set; }
+        public double Price { get; set; }
+        public string MovieImageUrl { get; set; }
+        public DateTime StartingDate { get; set; }
+        public DateTime EndingDate { get; set; }
+
+        //Relations
+
+        public List<Actors_Movie> Actors_Movies { get; set; }
+
+        // for cinema
+        public int CinemaId { get; set; }
+        [ForeignKey("CinemaId")]
+        public Cinema Cinema { get; set; }
+
+        // for procuder
+        public int ProducerId { get; set; }
+        [ForeignKey("ProducerId")]
+
+        public Producer Producer { get; set; }
     }
 }
