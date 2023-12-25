45346f9da52fd9f705a04b6b35b1bba91d1b1930
erenerdogan98 <eren.erdogannn@hotmail.com>
2023-12-25 13:58:27 +0300

    Model realtionship created

--- NTier_ECommerce_DAL/Context/Context.cs
+++ NTier_ECommerce_DAL/Context/Context.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NTier_ECommerce_Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +19,21 @@ namespace NTier_ECommerce_DAL.Context
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            modelBuilder.Entity<Actors_Movie>().HasKey(x => new
+            {
+                x.ActorId,
+                x.MovieId
+            });
+            modelBuilder.Entity<Actors_Movie>().HasOne(x => x.Movie).WithMany(x => x.Actors_Movies).HasForeignKey(x => x.MovieId);
+            modelBuilder.Entity<Actors_Movie>().HasOne(x => x.Actor).WithMany(x => x.Actors_Movies).HasForeignKey(x => x.ActorId);
+            base.OnModelCreating(modelBuilder);
+            /* Explanation 
+* HasKey => It is the method by which the combined key is determined.
+
+*HasOne and WithMany: Specifies relationships. The Actor_Movie table is associated with both a Movie and an Actor. 
+These methods are important in determining and directing the relationship.
+
+*HasForeignKey: Specifies foreign key relationships.*/
         }
     }
 }
