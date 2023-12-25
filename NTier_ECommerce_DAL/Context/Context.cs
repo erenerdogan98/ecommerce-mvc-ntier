a6bbdd1505034dad9073faf314f00019ddbb1d41
erenerdogan98 <eren.erdogannn@hotmail.com>
2023-12-25 13:52:40 +0300

    Context class created for database connection..

---
+++ NTier_ECommerce_DAL/Context/Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier_ECommerce_DAL.Context
+{
+    public class Context : DbContext
+    {
+        string connectionString = "server = DESKTOP-57R498V\\SQLEXPRESS01;database = ntierecommerce; integrated security = true;TrustServerCertificate=true;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
