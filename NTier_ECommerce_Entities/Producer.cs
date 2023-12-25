3d19dcd562734f077ff440117ef6dbeb0f34e930
erenerdogan98 <eren.erdogannn@hotmail.com>
2023-12-25 04:59:38 +0300

    Producer models created

---
+++ NTier_ECommerce_Entities/Producer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier_ECommerce_Entities
+{
+    public class Producer
+    {
+        [Key]
+        public int MyProperty { get; set; }
+        public string NameSurname { get; set; }
+        public string ProfileUrl { get; set; }
+        public string Biography { get; set; }
+    }
+}
