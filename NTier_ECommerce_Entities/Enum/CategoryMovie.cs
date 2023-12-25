fe526fa5027139c8f3ed5cc928d4f3308b4f753c
erenerdogan98 <eren.erdogannn@hotmail.com>
2023-12-25 15:04:41 +0300

    Enum for movie categories created

---
+++ NTier_ECommerce_Entities/Enum/CategoryMovie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier_ECommerce_Entities.Enum
+{
+    public enum CategoryMovie
+    {
+        Action = 1,
+        Drama,
+        Comedy,
+        Trajedy,
+        Documentary,
+        Cartoon,
+        Horror
+    }
+}
