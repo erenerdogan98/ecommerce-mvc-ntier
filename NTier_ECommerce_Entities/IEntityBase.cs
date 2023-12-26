b767b8a3ae5af46662bad43be69b38cf28d0c64b
erenerdogan98 <eren.erdogannn@hotmail.com>
2023-12-26 12:57:55 +0300

    interface for common id prop

---
+++ NTier_ECommerce_Entities/IEntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier_ECommerce_Entities
+{
+    public interface IEntityBase
+    {
+        public int Id { get; set; }
+
+    }
+}
