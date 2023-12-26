c7d3683922684911e46f48563570cefa9b58b6cf
erenerdogan98 <eren.erdogannn@hotmail.com>
2023-12-26 13:00:59 +0300

    migration added and updated database

---
+++ NTier_ECommerce_DAL/Migrations/20231226100026_IEntityInherited.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NTier_ECommerce_DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class IEntityInherited : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MyProperty",
+                table: "Producers",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryMovie",
+                table: "Movies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryMovie",
+                table: "Movies");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Producers",
+                newName: "MyProperty");
+        }
+    }
+}
