b18390fc60bc5e292668ae07107f4d7e78306ae0
erenerdogan98 <eren.erdogannn@hotmail.com>
2023-12-29 22:49:02 +0300

    business operations created ..

---
+++ NTier_Ecommerce_BLL/Concrete/ProducerManager.cs
@@ -0,0 +1,29 @@
+﻿using NTier_Ecommerce_BLL.Abstract;
+using NTier_ECommerce_DAL.EFRepository;
+using NTier_ECommerce_Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier_Ecommerce_BLL.Concrete
+{
+    public class ProducerManager : IProducerService
+    {
+        private readonly EFProducerRepository eFProducerRepository;
+        public ProducerManager(EFProducerRepository eFProducerRepository)
+        {
+            this.eFProducerRepository = eFProducerRepository ?? throw new ArgumentNullException(nameof(eFProducerRepository)); ;
+        }
+
+        public Task AddProducer(Producer producer) => eFProducerRepository.AddAsync(producer);
+
+        public Task Delete(Producer producer) => eFProducerRepository.DeleteAsync(producer.Id);
+        public Task<IEnumerable<Producer>> GetAll() => eFProducerRepository.GetAllAsync();
+
+        public Task<Producer> GetProducer(int id) => eFProducerRepository.GetByIdAsync(id);
+
+        public Task Update(Producer producer) => eFProducerRepository.UpdateAsync(producer.Id, producer);
+    }
+}
