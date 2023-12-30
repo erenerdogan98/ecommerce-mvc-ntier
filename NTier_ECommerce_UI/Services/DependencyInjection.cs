9630304900b652d8c022b7fbbd8f246926fd7ffd
erenerdogan98 <eren.erdogannn@hotmail.com>
2023-12-30 21:31:07 +0300

    Controlling dependencies in one class

---
+++ NTier_ECommerce_UI/Services/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using NTier_Ecommerce_BLL.Abstract;
+using NTier_Ecommerce_BLL.Concrete;
+using NTier_ECommerce_DAL.Database;
+
+namespace NTier_ECommerce_UI.Services
+{
+    public static class DependencyInjection
+    {
+        public static void Configure(this IServiceCollection services)
+        {
+            // I add services 
+            services.AddDbContext<Context>();
+
+            services.AddScoped<IActorsService, ActorsManager>();
+            services.AddScoped<IMovieService, MovieManager>();
+            services.AddScoped<ICinemaService, CinemaManager>();
+            services.AddScoped<IProducerService, ProducerManager>();
+        }
+    }
+}
