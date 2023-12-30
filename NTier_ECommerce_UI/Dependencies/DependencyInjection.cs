e59a30ebc04c76290ef65ee47f012fc70bd26b75
erenerdogan98 <eren.erdogannn@hotmail.com>
2023-12-30 21:14:01 +0300

    For controlling dependency injections in a static class

---
+++ NTier_ECommerce_UI/Dependencies/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NTier_Ecommerce_BLL.Abstract;
+using NTier_Ecommerce_BLL.Concrete;
+using NTier_ECommerce_DAL.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier_ECommerce_DAL.Dependencies
+{
+    public static class DependencyInjection
+    {
+        public static void Configure(this IServiceCollection services)
+        {
+            // I add services 
+            services.AddDbContext<Context>();
+
+            services.AddScoped<IActorsService, ActorsManager>();
+            services.AddScoped<IMovieService, MovieManager>();
+            services.AddScoped<ICinemaService, CinemaManager>();
+            services.AddScoped<IProducerService, ProducerManager>();
+        }
+    }
+}
