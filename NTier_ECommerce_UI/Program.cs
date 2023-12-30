b64933e1517e6cd1eb73185b17412a3487a6278f
erenerdogan98 <eren.erdogannn@hotmail.com>
2023-12-30 21:32:14 +0300

    Adding Configure method to program.cs ..

--- NTier_ECommerce_UI/Program.cs
+++ NTier_ECommerce_UI/Program.cs
@@ -1,7 +1,12 @@
+using NTier_ECommerce_UI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Services configurations
+builder.Services.Configure();
 
 var app = builder.Build();
 
