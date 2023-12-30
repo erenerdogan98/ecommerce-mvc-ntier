71902650b0a2e6ef146a558383f057b90c59ac10
erenerdogan98 <eren.erdogannn@hotmail.com>
2023-12-30 21:09:30 +0300

    changin namespace name

--- NTier_Ecommerce_BLL/Abstract/ICinemaService.cs
+++ NTier_Ecommerce_BLL/Abstract/ICinemaService.cs
@@ -1,12 +1,8 @@
 ﻿using NTier_ECommerce_DAL.GenericRepository;
 using NTier_ECommerce_Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace NTier_ECommerce_DAL.Abstract
+
+namespace NTier_ECommerce_BLL.Abstract
 {
     public interface ICinemaService 
     {
