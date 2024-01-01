19931715ac0925bc54564dd52e7689fcb61ac0b6
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-01 23:34:51 +0300

    instead of EntityFramework repository , use ICinemaDAL

--- NTier_Ecommerce_BLL/Concrete/CinemaManager.cs
+++ NTier_Ecommerce_BLL/Concrete/CinemaManager.cs
@@ -1,4 +1,5 @@
 ﻿using NTier_Ecommerce_BLL.Abstract;
+using NTier_ECommerce_DAL.Abstract;
 using NTier_ECommerce_DAL.EFRepository;
 using NTier_ECommerce_Entities;
 
@@ -6,20 +7,21 @@ namespace NTier_Ecommerce_BLL.Concrete
 {
     public class CinemaManager : ICinemaService
     {
-        EFCinemaRepostiory _eFCinemaRepostiory;
-        public CinemaManager(EFCinemaRepostiory eFCinemaRepostiory)
+        //EFCinemaRepostiory _eFCinemaRepostiory;
+        private readonly ICinemaDAL _cinemaDAL;
+        public CinemaManager(ICinemaDAL cinemaDAL)
         {
-            _eFCinemaRepostiory = eFCinemaRepostiory ?? throw new ArgumentNullException(nameof(_eFCinemaRepostiory)); ;
+            _cinemaDAL = cinemaDAL ?? throw new ArgumentNullException(nameof(cinemaDAL)); ;
         }
-        public Task AddCinema(Cinema cinema) => _eFCinemaRepostiory.AddAsync(cinema);
+        public Task AddCinema(Cinema cinema) => _cinemaDAL.AddAsync(cinema);
 
-        public Task<IEnumerable<Cinema>> GetAllCinema() => _eFCinemaRepostiory.GetAllAsync();
+        public Task<IEnumerable<Cinema>> GetAllCinema() => _cinemaDAL.GetAllAsync();
 
-        public Task<Cinema> GetCinemaById(int id) => _eFCinemaRepostiory.GetByIdAsync(id);
+        public Task<Cinema> GetCinemaById(int id) => _cinemaDAL.GetByIdAsync(id);
 
-        public Task RemoveCinema(Cinema cinema) => _eFCinemaRepostiory.DeleteAsync(cinema.Id);
+        public Task RemoveCinema(Cinema cinema) => _cinemaDAL.DeleteAsync(cinema.Id);
 
-        public Task UpdateCinema(Cinema cinema) => _eFCinemaRepostiory.UpdateAsync(cinema.Id, cinema);
+        public Task UpdateCinema(Cinema cinema) => _cinemaDAL.UpdateAsync(cinema.Id, cinema);
         
     }
 }
