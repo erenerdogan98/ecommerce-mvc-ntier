2fd9848590ff28cf16b884ffee97da42348ea85a
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-02 20:15:26 +0300

    scoped created ...

--- NTier_ECommerce_UI/Services/DependencyInjection.cs
+++ NTier_ECommerce_UI/Services/DependencyInjection.cs
@@ -1,20 +1,29 @@
 ﻿using NTier_Ecommerce_BLL.Abstract;
 using NTier_Ecommerce_BLL.Concrete;
+using NTier_ECommerce_DAL.Abstract;
 using NTier_ECommerce_DAL.Database;
+using NTier_ECommerce_DAL.EFRepository;
 
 namespace NTier_ECommerce_UI.Services
 {
     public static class DependencyInjection
     {
-        public static void Configure(this IServiceCollection services)
+        public static void ConfigureMyServices(this IServiceCollection services)
         {
             // I add services 
             services.AddDbContext<Context>();
 
             services.AddScoped<IActorsService, ActorsManager>();
+            services.AddScoped<IActorsDAL, EFActorsRepository>();
+
             services.AddScoped<IMovieService, MovieManager>();
+            services.AddScoped<IMovieDAL, EFMovieRepository>();
+
             services.AddScoped<ICinemaService, CinemaManager>();
-            services.AddScoped<IProducerService, ProducerManager>();
+            services.AddScoped<ICinemaDAL, EFCinemaRepostiory>();
+
+            services.AddScoped<IProducerService, ProducerManager>();        
+            services.AddScoped<IProducerDAL, EFProducerRepository>();
         }
     }
 }
