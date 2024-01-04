441caecd84c3f68e76c13d6798902e5396afea82
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-04 17:13:13 +0300

    order entity created

---
+++ NTier_ECommerce_Entities/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier_ECommerce_Entities
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; }
+
+    }
+}
