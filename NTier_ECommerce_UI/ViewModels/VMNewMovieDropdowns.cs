e92a7c29b5495d5d283291482c54746a90251f44
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-05 18:25:02 +0300

    view model for user can choose dropdown list

---
+++ NTier_ECommerce_UI/ViewModels/VMNewMovieDropdowns.cs
@@ -0,0 +1,23 @@
+﻿using NTier_ECommerce_Entities;
+
+namespace NTier_ECommerce_UI.ViewModels
+{
+    public class VMNewMovieDropdowns
+    {
+        public VMNewMovieDropdowns()
+        {
+            Producers = new List<Producer>();
+            Cinemas = new List<Cinema>();
+            Actors = new List<Actors>();
+        }
+
+        public List<Producer> Producers { get; set; }
+        public List<Cinema> Cinemas { get; set; }
+        public List<Actors> Actors { get; set; }
+    }
+}
+/* 
+    This type of ViewModel is usually created by a controller and used by a view. For example,
+it can be used on a movie add page to offer the user the ability to choose from dropdown lists.
+The Controller assigns the required data to the ViewModel and allows the user to make selections while this ViewModel is used by the View.
+ */
