5808781502b7b63dfeb92075428fecf23fc5e509
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-05 03:17:04 +0300

    For remove chaning parameter only id

--- NTier_Ecommerce_BLL/Abstract/IActorsService.cs
+++ NTier_Ecommerce_BLL/Abstract/IActorsService.cs
@@ -5,7 +5,7 @@ namespace NTier_Ecommerce_BLL.Abstract
     public interface IActorsService
     {
         Task AddActorAsync(Actors actor);
-        Task RemoveActorAsync(int id,Actors actor);
+        Task RemoveActorAsync(int id);
         Task UpdateActorAsync(int id,Actors actor);
         Task<IEnumerable<Actors>> GetAllActorsAsync();
         Task<Actors> GetActorByIdAsync(int actorId);
