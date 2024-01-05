38da8f1a17c971b3ae1be775c3c175ebf0b7741f
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-06 02:53:51 +0300

    Inherited IGenericService

--- NTier_Ecommerce_BLL/Abstract/IActorsService.cs
+++ NTier_Ecommerce_BLL/Abstract/IActorsService.cs
@@ -2,12 +2,12 @@
 
 namespace NTier_Ecommerce_BLL.Abstract
 {
-    public interface IActorsService
+    public interface IActorsService : IGenericService<Actors>
     {
-        Task AddActorAsync(Actors actor);
-        Task RemoveActorAsync(int id);
-        Task UpdateActorAsync(int id,Actors actor);
-        Task<IEnumerable<Actors>> GetAllActorsAsync();
-        Task<Actors> GetActorByIdAsync(int actorId);
+        //Task AddActorAsync(Actors actor);
+        //Task RemoveActorAsync(int id);
+        //Task UpdateActorAsync(int id,Actors actor);
+        //Task<IEnumerable<Actors>> GetAllActorsAsync();
+        //Task<Actors> GetActorByIdAsync(int actorId);
     }
 }
