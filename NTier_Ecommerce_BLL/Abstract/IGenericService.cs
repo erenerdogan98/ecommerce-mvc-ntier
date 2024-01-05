89edc85106f11de0bd1cac6da419ef344cd9bcac
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-06 02:52:33 +0300

    For Generic services , I define this interface

---
+++ NTier_Ecommerce_BLL/Abstract/IGenericService.cs
@@ -0,0 +1,12 @@
+﻿// for Generic methods
+namespace NTier_Ecommerce_BLL.Abstract
+{
+    public interface IGenericService<T>
+    {
+        Task AddAsync(T entity);
+        Task DeleteAsync(int id);
+        Task<T> UpdateAsync(int id,T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+    }
+}
