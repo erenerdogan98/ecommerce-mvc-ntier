a467ffd7489f5669ba4628aef757e17da6d44957
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-05 18:21:17 +0300

    ups , forget to create this folder in UI layer , cut and paste this folder to UI layer

---
+++ NTier_ECommerce_UI/ViewModels/VMLogin.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace NTier_ECommerce_Entities.ViewModels
+{
+    public class VMLogin
+    {
+            [Display(Name = "Email address")]
+            [Required(ErrorMessage = "Email address is required")]
+            public string EmailAddress { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+    }
+}
