a55eb4ad0d31345e8ed89d6eb2f25310a0cca81f
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-05 22:11:13 +0300

    Last changing added and updated database

--- NTier_ECommerce_DAL/Migrations/20240105190953_OrdersAdded.Designer.cs
+++ NTier_ECommerce_DAL/Migrations/20240105190953_OrdersAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NTier_ECommerce_DAL.Database;
 
@@ -11,9 +12,11 @@ using NTier_ECommerce_DAL.Database;
 namespace NTier_ECommerce_DAL.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20240105190953_OrdersAdded")]
+    partial class OrdersAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
