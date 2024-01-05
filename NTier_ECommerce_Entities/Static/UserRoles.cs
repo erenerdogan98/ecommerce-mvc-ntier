abf99f6bf0e174fe19a87614fffe0f0fc170efe7
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-05 18:14:09 +0300

    static class for user roles

--- NTier_ECommerce_Entities/Static/UserRoles.cs
+++ NTier_ECommerce_Entities/Static/UserRoles.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// for authantication operations 
 
 namespace NTier_ECommerce_Entities.Static
 {
