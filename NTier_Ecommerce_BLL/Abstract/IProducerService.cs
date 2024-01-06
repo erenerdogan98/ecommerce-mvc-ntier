c61af77a3eb8578bc24f548a976f48f02420b2ae
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-06 03:25:18 +0300

    Generic service inherited

--- NTier_Ecommerce_BLL/Abstract/IProducerService.cs
+++ NTier_Ecommerce_BLL/Abstract/IProducerService.cs
@@ -1,18 +1,14 @@
-﻿using NTier_ECommerce_Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using NTier_ECommerce_Entities;
 
 namespace NTier_Ecommerce_BLL.Abstract
 {
-    public interface IProducerService
+    public interface IProducerService : IGenericService<Producer>
     {
-        Task AddProducer(Producer producer);
-        Task Update(Producer producer);
-        Task Delete(Producer producer);
-        Task<IEnumerable<Producer>> GetAll();
-        Task<Producer> GetProducer(int id);
+        //Task AddProducer(Producer producer);
+        //Task Update(Producer producer);
+        //Task Delete(Producer producer);
+        //Task<IEnumerable<Producer>> GetAll();
+        //Task<Producer> GetProducer(int id);
     }
 }
