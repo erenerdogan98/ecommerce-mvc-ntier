15bed6474cf2025ff0b29612309dadc2a38484ff
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-15 18:28:28 +0300

    DTO Layer created

---
+++ DTOLayer/MovieDto/VMNewMovieDropdownsDTO.cs
@@ -0,0 +1,23 @@
+﻿using NTier_ECommerce_Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOLayer.MovieDto
+{
+    public class VMNewMovieDropdownsDTO
+    {
+        public VMNewMovieDropdownsDTO()
+        {
+            Producers = new List<Producer>();
+            Cinemas = new List<Cinema>();
+            Actors = new List<Actors>();
+        }
+
+        public List<Producer> Producers { get; set; }
+        public List<Cinema> Cinemas { get; set; }
+        public List<Actors> Actors { get; set; }
+    }
+}
