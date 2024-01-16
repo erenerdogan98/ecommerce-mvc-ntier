a5414d1abcbf4fc1592fcc81a6bd36bbf5735bec
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-16 17:07:51 +0300

    for ef , shopping cart item added

---
+++ NTier_ECommerce_DAL/EFRepository/EFShoppingCartRepository.cs
@@ -0,0 +1,19 @@
+﻿using NTier_ECommerce_DAL.Abstract;
+using NTier_ECommerce_DAL.Database;
+using NTier_ECommerce_DAL.GenericRepository;
+using NTier_ECommerce_Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier_ECommerce_DAL.EFRepository
+{
+    public class EFShoppingCartRepository : GenericRepository<ShoppingCartItem> , IShoppingDAL
+    {
+        public EFShoppingCartRepository(Context context) : base(context)
+        {         
+        }
+    }
+}
