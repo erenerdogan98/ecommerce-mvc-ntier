54d4004066dee2b99fd9a8cb225d932b1fb005ba
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-16 17:52:37 +0300

    forget to inherite IEntitybase and it added

--- NTier_ECommerce_Entities/ShoppingCartItem.cs
+++ NTier_ECommerce_Entities/ShoppingCartItem.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace NTier_ECommerce_Entities
 {
-    public class ShoppingCartItem
+    public class ShoppingCartItem : IEntityBase
     {
         [Key]
         public int Id { get; set; }
