b2bcccf1ef61c3be0b9e173f8453f216a24fa9f6
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-16 17:09:26 +0300

    for shoppingcart service (interface) added

---
+++ NTier_Ecommerce_BLL/Abstract/IShoppingCartService.cs
@@ -0,0 +1,13 @@
+﻿using NTier_Ecommerce_BLL.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier_Ecommerce_BLL.Abstract
+{
+    public interface IShoppingCartService : IGenericService<ShoppingCart>
+    {
+    }
+}
