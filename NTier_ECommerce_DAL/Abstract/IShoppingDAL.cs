33aba02cb6e2be9accc1d4967b1df4e755f1d2f6
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-18 16:14:04 +0300

    Some speacial props added

--- NTier_ECommerce_DAL/Abstract/IShoppingDAL.cs
+++ NTier_ECommerce_DAL/Abstract/IShoppingDAL.cs
@@ -10,6 +10,9 @@ namespace NTier_ECommerce_DAL.Abstract
 {
     public interface IShoppingDAL : IGenericRepository<ShoppingCartItem>
     {
-
+        public double GetShoppingCartTotal();
+        public void AddItemToCart(Movie movie);
+        public void RemoveItemFromCart(Movie movie);
+        public  Task ClearShoppingCartAsyn();
     }
 }
