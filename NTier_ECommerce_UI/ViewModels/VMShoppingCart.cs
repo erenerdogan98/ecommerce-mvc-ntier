3f7a4d4fbedc9f52405bf240278b110bf523bf04
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-18 17:43:52 +0300

    Shopping Cart view model added

---
+++ NTier_ECommerce_UI/ViewModels/VMShoppingCart.cs
@@ -0,0 +1,10 @@
+﻿using NTier_Ecommerce_BLL.Cart;
+
+namespace NTier_ECommerce_UI.ViewModels
+{
+    public class VMShoppingCart
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
+    }
+}
