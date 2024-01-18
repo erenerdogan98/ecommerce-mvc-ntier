861bb1074ce03b138b0a8396e54b117ea3b0eeaf
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-18 16:14:28 +0300

    After changed IShoppingDAL , implemented and added methods

--- NTier_ECommerce_DAL/EFRepository/EFShoppingCartRepository.cs
+++ NTier_ECommerce_DAL/EFRepository/EFShoppingCartRepository.cs
@@ -1,4 +1,5 @@
-﻿using NTier_ECommerce_DAL.Abstract;
+﻿using Microsoft.EntityFrameworkCore;
+using NTier_ECommerce_DAL.Abstract;
 using NTier_ECommerce_DAL.Database;
 using NTier_ECommerce_DAL.GenericRepository;
 using NTier_ECommerce_Entities;
@@ -10,10 +11,65 @@ using System.Threading.Tasks;
 
 namespace NTier_ECommerce_DAL.EFRepository
 {
-    public class EFShoppingCartRepository : GenericRepository<ShoppingCartItem> , IShoppingDAL
+    public class EFShoppingCartRepository : GenericRepository<ShoppingCartItem>, IShoppingDAL
     {
-        public EFShoppingCartRepository(Context context) : base(context)
-        {         
+        private readonly Context _context;
+        private readonly string _shoppingCartId;
+        public EFShoppingCartRepository(Context context, string shoppingCartId = null) : base(context)
+        {
+            _shoppingCartId = shoppingCartId;
         }
+
+        public void AddItemToCart(Movie movie)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(n => n.Movie.Id == movie.Id && n.ShoppingCartId == _shoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem()
+                {
+                    ShoppingCartId = _shoppingCartId,
+                    Movie = movie,
+                    Amount = 1
+                };
+
+                _context.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+            SaveChanges();
+        }
+
+        public async Task ClearShoppingCartAsyn()
+        {
+            var items = await _context.ShoppingCartItems.Where(n => n.ShoppingCartId == _shoppingCartId).ToListAsync();
+            _context.ShoppingCartItems.RemoveRange(items);
+            SaveChangesAsync();
+        }
+
+        public double GetShoppingCartTotal() => _context.ShoppingCartItems.Where(n => n.ShoppingCartId == _shoppingCartId).Select(n => n.Movie.Price * n.Amount).Sum();
+
+        public void RemoveItemFromCart(Movie movie)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(n => n.Movie.Id == movie.Id && n.ShoppingCartId == _shoppingCartId);
+
+            if (shoppingCartItem != null)
+            {
+                if (shoppingCartItem.Amount > 1)
+                {
+                    shoppingCartItem.Amount--;
+                }
+                else
+                {
+                    _context.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+            }
+            SaveChanges();
+        }
+        private void SaveChanges() => _context.SaveChanges();
+
+        private async Task SaveChangesAsync() => await _context.SaveChangesAsync();
     }
 }
