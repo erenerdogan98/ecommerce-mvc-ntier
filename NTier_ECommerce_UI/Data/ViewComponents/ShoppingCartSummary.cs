8aba9940550a6484f0e2f2214a5b84bd4f373164
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-19 23:38:51 +0300

    added viewcomponent for shoppingcartsummary

---
+++ NTier_ECommerce_UI/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NTier_Ecommerce_BLL.Cart;
+
+namespace NTier_ECommerce_UI.Data.ViewComponents
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            return View(items.Count);
+        }
+    }
+}
