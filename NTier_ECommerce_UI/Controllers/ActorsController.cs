22e0086f933413cd533ced31dc8dc106a286b99d
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-19 17:58:13 +0300

    refactoring and added authorize

--- NTier_ECommerce_UI/Controllers/ActorsController.cs
+++ NTier_ECommerce_UI/Controllers/ActorsController.cs
@@ -1,85 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using NTier_Ecommerce_BLL.Abstract;
 using NTier_ECommerce_Entities;
+using NTier_ECommerce_Entities.Static;
+using System.Threading.Tasks;
 
 namespace NTier_ECommerce_UI.Controllers
 {
+    [Authorize(Roles = UserRoles.Admin)]
     public class ActorsController : Controller
     {
         private readonly IActorsService _actorsService;
+
         public ActorsController(IActorsService actorsService)
         {
             _actorsService = actorsService;
         }
-        public async Task<IActionResult> Index()
-        {
-            var data = await _actorsService.GetAllAsync();
-            return View(data);
-        }
 
-        // Get : Actors/AddActor
-        [HttpPost]
-        public async Task<IActionResult> AddActor([Bind("NameSurname,ProfileUrl,Biography")] Actors actor)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(actor);
-            }
-            await _actorsService.AddAsync(actor);
-            return RedirectToAction(nameof(Index));
-        }
-        // Get: Actors/Details/1
-        public async Task<IActionResult> Details(int id)
-        {
-            var actorDetails = await _actorsService.GetByIdAsync(id);
-
-            if (actorDetails == null)
-                return View("NotFound");
-            return View(actorDetails);
-        }
-
-        //Get : Actors/Edit/1
-        public async Task<IActionResult> Edit(int id)
-        {
-            var actorDetails = await _actorsService.GetByIdAsync(id);
-
-            if (actorDetails == null)
-                return View("NotFound");
-            return View(actorDetails);
-
-        }
+        public async Task<IActionResult> Index() => View(await _actorsService.GetAllAsync());
 
         [HttpPost]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,NameSurname,ProfileUrl,Biography")] Actors actor)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(actor);
-            }
-            await _actorsService.UpdateAsync(id, actor);
-            return RedirectToAction(nameof(Index));
-        }
+        public async Task<IActionResult> AddActor([Bind("NameSurname,ProfileUrl,Biography")] Actors actor) =>
+            await ProcessFormSubmissionAsync(actor, () => _actorsService.AddAsync(actor), nameof(Index));
 
-        //Get : Actors/Delete/1
-        public async Task<IActionResult> Delete(int id)
-        {
-            var actorDetails = await _actorsService.GetByIdAsync(id);
+        public async Task<IActionResult> Details(int id) => await GetViewResultForEntityAsync(id);
 
-            if (actorDetails == null)
-                return View("NotFound");
-            return View(actorDetails);
+        public async Task<IActionResult> Edit(int id) => await GetViewResultForEntityAsync(id);
 
-        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,NameSurname,ProfileUrl,Biography")] Actors actor) =>
+            await ProcessFormSubmissionAsync(actor, () => _actorsService.UpdateAsync(id, actor), nameof(Index));
+
+        public async Task<IActionResult> Delete(int id) => await GetViewResultForEntityAsync(id);
 
         [HttpPost, ActionName("Delete")]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(int id) =>
+            await ProcessFormSubmissionAsync(null, () => _actorsService.DeleteAsync(id), nameof(Index));
+
+        private async Task<IActionResult> GetViewResultForEntityAsync(int id)
         {
             var actorDetails = await _actorsService.GetByIdAsync(id);
-            if (actorDetails == null) return View("NotFound");
+            return actorDetails == null ? View("NotFound") : View(actorDetails);
+        }
 
-            await _actorsService.DeleteAsync(id);
-            return RedirectToAction(nameof(Index));
+        private async Task<IActionResult> ProcessFormSubmissionAsync(Actors actor, Func<Task> action, string redirectToAction)
+        {
+            if (!ModelState.IsValid) return View(actor);
+
+            await action.Invoke();
+            return RedirectToAction(redirectToAction);
         }
     }
 }
- 
